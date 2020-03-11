9e81c57f3fe148e3395f56fc87c395bb84fa0941
Victoria Hawoldar <victoria@hawoldar.co.uk>
2020-03-11 11:00:33 +0000

    improve register form

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,22 @@ router.get("/register", function(req, res){
 router.post("/register", function(req, res){
     req.body.username = req.body.username.toLowerCase();
     var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            req.flash("error", err.message);
-            res.render("register")
-        }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome " + user.username);
-            res.redirect("/campsites");
+    if(req.body.password === req.body.confirm){
+        User.register(newUser, req.body.password, function(err, user){
+            if(err){
+                req.flash("error", err.message);
+                res.render("register")
+            }
+            passport.authenticate("local")(req, res, function(){
+                req.flash("success", "Welcome " + user.username);
+                res.redirect("/campsites");
+            });
         });
-    });
+    } else {
+        req.flash("error", "Passwords do not match")
+        res.redirect("/register");
+    }
+    
 });
 router.get("/login", function(req, res){
     res.render("login");
